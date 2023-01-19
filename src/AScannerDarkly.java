d061bb724402583e61db4f92e1d567a8afdc046e
Adam <adam.ewing.nz@gmail.com>
2023-01-20 07:35:49 +0800

    Added single-line comments for instructional purposes.

--- src/AScannerDarkly.java
+++ src/AScannerDarkly.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class AScannerDarkly {
+public class AScannerDarkly {
+
+    public static void main(String[] args) {
+
+        // 1. Create a Scanner object called 'scanner' that reads user input.
+
+        // 2. Print out a message that says "Please enter your first name: "
+
+        // 3. Store the input into a variable called 'firstName'.
+
+        // 4. Print out a message that says "Please enter your last name: "
+
+        // 5. Store the input into a variable called 'lastName'.
+
+        // 6. Print out a message that says "Please enter your age: "
+
+        // 7. Store the input into a variable called 'age'.
+
+        // 8. Store all the details of the user into a variable called 'userInfo'.
+
+            /* The format for this String should be, for example, as follows:
+
+                First name: Keanu
+                Last name: Reeves
+                Age: 58
+
+            */
+
+        // 9. Print out the variable 'userInfo'.
+
+        // 10. Close the Scanner object.
+
+    }
+
 }
